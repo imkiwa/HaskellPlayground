87dc81beea9b542668de392bd0d637019a896cef
imkiva <libkernelpanic@gmail.com>
2019-10-18 12:38:56 +0800

    lib: hamming numbers

--- app/Prove.hs
+++ app/Prove.hs
@@ -1,2 +1,45 @@
-module Prove where
+{-# LANGUAGE GADTs         #-}
+{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE TypeOperators #-}
 
+module Prove(plusCommutes) where
+
+-- | The natural numbers.
+data Z
+data S n
+
+-- | Predicate describing natural numbers.
+-- | This allows us to reason with `Nat`s.
+data Natural :: * -> * where
+    NumZ :: Natural Z
+    NumS :: Natural n -> Natural (S n)
+
+-- | Predicate describing equality of natural numbers.
+data Equal :: * -> * -> * where
+    EqlZ :: Equal Z Z
+    EqlS :: Equal n m -> Equal (S n) (S m)
+
+-- | Peano definition of addition.
+type family (:+:) (n :: *) (m :: *) :: *
+type instance Z :+: m = m
+type instance S n :+: m = S (n :+: m)
+
+-- | if a = b and b = c, then a = c.
+transitive :: Equal a b -> Equal b c -> Equal a c
+transitive EqlZ EqlZ = EqlZ
+transitive (EqlS e1) (EqlS e2) = EqlS $ transitive e1 e2
+
+-- | a + (b ++) = (a + b) ++
+lemma :: Natural m -> Natural n -> Equal (m :+: S n) (S (m :+: n))
+lemma NumZ NumZ = EqlS EqlZ
+lemma NumZ (NumS n) = EqlS $ lemma NumZ n
+lemma (NumS m) n = EqlS $ lemma m n
+
+-- | a + zero = zero + a
+-- | a + (b ++) = (a + b) ++
+-- | (b ++) + a = (b + a) ++
+-- | then a + b = b + a
+plusCommutes :: Natural a -> Natural b -> Equal (a :+: b) (b :+: a)
+plusCommutes NumZ NumZ = EqlZ
+plusCommutes (NumS m) NumZ = EqlS $ plusCommutes m NumZ
+plusCommutes m (NumS n) = transitive (lemma m n) $ EqlS $ plusCommutes m n
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,7 @@
 module Lib where
 
+import Data.List(group)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
@@ -9,3 +11,24 @@ at x i = (head . drop i) x
 iterate' :: (a -> a) -> a -> [a]
 iterate' f i = i : iterate' f (f i)
 
+delete' :: Eq a => a -> [a] -> [a]
+delete' _ [] = []
+delete' t (x:xs) =
+  let rest = delete' t xs
+   in if t == x
+        then rest
+        else x : rest
+
+mergeSort :: Ord a => [a] -> [a] -> [a]
+mergeSort (x:xs) (y:ys)
+  | x > y = y : mergeSort (x : xs) ys
+  | otherwise = x : mergeSort xs (y : ys)
+
+mergeSort3 :: Ord a => [a] -> [a] -> [a] -> [a]
+mergeSort3 x y = mergeSort (mergeSort x y)
+
+uniq :: Eq a => [a] -> [a]
+uniq = map head . group
+
+ham :: (Num a, Ord a) => [a]
+ham = 1 : uniq (mergeSort3 (map (* 2) ham) (map (* 3) ham) (map (* 5) ham))
